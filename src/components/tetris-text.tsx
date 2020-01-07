058c3c063b85011c491a8e80b309f79bf13f16f9
SimonTheBarnett <professionalsoftwaretesting@gmail.com>
2020-01-07 21:21:57 +0000

    base version of tetris

---
+++ src/components/tetris-text.tsx
@@ -0,0 +1,12 @@
+// Import React
+import React from 'react'
+
+function TetrisText() {
+        return (
+            <div className="tetris-banner">
+                <h1 className="tetris-banner-text">Yet Another Tetris Clone</h1>
+            </div>
+        )
+}
+
+export default TetrisText;
